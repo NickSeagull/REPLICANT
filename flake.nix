12fea7bf5786078aa855ba8640ed0e0c2296f0c0
Nikita Tchayka <github@nickseagull.dev>
2025-03-15 23:08:45 +0000

    format

--- flake.nix
+++ flake.nix
@@ -1,27 +1,28 @@
 {
-    description = "REPLICANT";
+  description = "REPLICANT";
 
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
-        home-manager = {
-          url = "github:nix-community/home-manager/master";
-          inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      homeConfigurations = {
+        nick =
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
           };
       };
-
-    outputs = {nixpkgs, home-manager, ...}: 
-      let
-        lib = nixpkgs.lib;
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-      in {
-          homeConfigurations = {
-            nick = 
-                home-manager.lib.homeManagerConfiguration {
-                    inherit pkgs;
-                    modules = [ ./home.nix ];
-                  };
-              };
-        };
-  }
+    };
+}
--- home.nix
+++ home.nix
@@ -1,27 +1,27 @@
 { lib, pkgs, ... }:
 {
-    home = {
-        packages = with pkgs; [
-          bitwarden-cli
-          nixpkgs-fmt
-        ];
+  home = {
+    packages = with pkgs; [
+      bitwarden-cli
+      nixpkgs-fmt
+    ];
 
 
-        username = "nick";
-        homeDirectory = "/home/nick/";
-        stateVersion = "24.11";
-      };
-        programs = {
-            git = {
-                enable = true;
-                userEmail = "github@nickseagull.dev";
-                userName = "Nikita Tchayka";
-              };
-            home-manager = {
-                enable = true;
-              };
-            fish = {
-                enable = true;
-              };
-          };
-  }
+    username = "nick";
+    homeDirectory = "/home/nick/";
+    stateVersion = "24.11";
+  };
+  programs = {
+    git = {
+      enable = true;
+      userEmail = "github@nickseagull.dev";
+      userName = "Nikita Tchayka";
+    };
+    home-manager = {
+      enable = true;
+    };
+    fish = {
+      enable = true;
+    };
+  };
+}
