34a3a828d95d145d27bcab5997966c61620feb2b
Nikita Tchayka <github@nickseagull.dev>
2025-03-15 22:57:46 +0000

    update

--- home.nix
+++ home.nix
@@ -5,11 +5,17 @@
           git
           bitwarden-cli
           nixpkgs-fmt
+          home-manager
         ];
+
+        programs = {
+            fish = {
+                enable = true;
+              };
+          };
 
         username = "nick";
         homeDirectory = "/home/nick/";
-
         stateVersion = "24.11";
       };
   }
